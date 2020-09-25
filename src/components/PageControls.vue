7a42daf70dd251bb04bb99fea811fdfe87ab6a07
emiliano <emiliano.0218@gmail.com>
2020-09-25 12:42:46 -0500

    agrego vuex

--- src/components/PageControls.vue
+++ src/components/PageControls.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row mt-2"> 
-    <div class="col form-group">
+    <div class="col-3 form-group">
       <select 
         class="form-control"
         v-on:change="changePageSize"
@@ -11,16 +11,39 @@
       </select>
     </div>
     <div class="text-right col">
-      <div class="btn-group mx-2">
-        <button
-          v-for="i in pageNumbers" v-bind:key="i" 
+      <button 
+        v-bind:disabled="currentPage == 1"
+        v-on:click="setCurrentPage(currentPage - 1)"
+        class="btn btn-secondary mx-1"
+        >
+        Previuos
+      </button>
+      <span
+        v-if="currentPage > 4">
+        <button v-on:click="setCurrentPage(1)" class="btn btn-secondary mx-1">1</button>
+        <span class="h4">...</span>
+      </span>
+      <span class="mx-1">
+        <button 
+          v-for="i in pageNumbers"
+          v-bind:key="i"
           class="btn btn-secondary"
           v-bind:class="{ 'btn-primary' : i == currentPage }"
           v-on:click="setCurrentPage(i)"
-          >
-          {{i}}
+          >{{i}}
         </button>
-      </div>
+      </span>
+      <span v-if="currentPage <= pageCount - 4">
+        <span class="h4">...</span>
+        <button 
+          v-on:click='setCurrentPage(pageCount)'
+          class="btn btn-secondary mx-1">{{pageCount}}</button>
+      </span>
+      <button 
+        v-bind:disabled="currentPage == pageCount"
+        v-on:click="setCurrentPage(currentPage + 1)"
+        class="btn btn-secondary mx-1"
+        >Next</button>
     </div>
   </div>
 </template>
@@ -33,7 +56,16 @@ export default {
     ...mapState(['currentPage']),
     ...mapGetters(['pageCount']),
     pageNumbers() {
-      return [...Array(this.pageCount +  1).keys()].slice(1)
+      // return [...Array(this.pageCount +  1).keys()].slice(1)
+      if(this.pageCount < 4){
+        return [...Array(this.pageCount +  1).keys()].slice(1);
+      }else if(this.currentPage <= 4){
+        return [1, 2, 3, 4, 5];
+      }else if(this.currentPage > this.pageCount - 4){
+        return [this.currentPage -1, this.currentPage, this.currentPage + 1];
+      }else{
+        return [this.currentPage - 1, this.currentPage, this.currentPage + 1];
+      }
     }
   }, 
   methods: {
